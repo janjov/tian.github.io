c094914cfa7a6ae6c0b876bad59ac75d892db581
janjov <114235006+janjov@users.noreply.github.com>
2022-11-25 08:26:15 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    titleWeb: "For Crush",
+    introTitle: 'Hello Crush',
+    introDesc: 'I have little message for you hehe',
+    btnIntro: 'Click',
+    title: 'Kamusta ka? I hope you are doing well hehe',
+    desc: 'Crush mo ba ako?',
+    btnYes: 'Yes',
+    btnNo: 'No',
+    question: 'Di nga? Crush mo ako?',
+    btnReply: 'Click mo',
+    reply: 'Crush din kita HAHA',
+    mess: 'Di ko alam sasabihin ko HAHA',
+    messDesc: 'Speechless na ako wala na',
+    btnAccept: 'Yun lang sana magustuhan mo HEHE'
+}
